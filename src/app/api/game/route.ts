b13f7de1e3c289f10dfd1d71540a44e15f2e8f7d
Eric Terrisse <eterrissebordas@gmail.com>
2023-10-18 00:12:17 -0500

    game call to api
    api returns questions and save on database

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,43 +1,101 @@
-// / api/game
-
-import { getAuthSession } from "@/lib/nextauth";
-import { NextResponse } from "next/server";
-import { quizCreationSchema } from "@/schemas/form/quiz";
-import { ZodError } from "zod";
 import { db } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import axios from "axios";
 
 export async function POST(req: Request, res: Response) {
 	try {
 		const session = await getAuthSession();
 		if (!session?.user) {
 			return NextResponse.json(
-				{
-					error: "You must be logged in",
-				},
+				{ error: "You must be logged in to create a game." },
 				{
 					status: 401,
 				}
 			);
 		}
 		const body = await req.json();
-		const { amount, topic, type } = quizCreationSchema.parse(body);
-        const game = db.game.create({
-            data: {
-                gameType: type,
-                timeStarted: new Date(),
-                userId: session.user.id,
-                topic
+		const { topic, type, amount } = quizCreationSchema.parse(body);
+		const game = await db.game.create({
+			data: {
+				gameType: type,
+				timeStarted: new Date(),
+				userId: session.user.id,
+				topic,
+			},
+		});
 
-            }
-        })
+		const { data } = await axios.post(
+			`${process.env.API_URL as string}/api/questions`,
+			{
+				amount,
+				topic,
+				type,
+			}
+		);
+
+		if (type === "mcq") {
+			type mcqQuestion = {
+				question: string;
+				answer: string;
+				option1: string;
+				option2: string;
+				option3: string;
+			};
+
+			const manyData = data.questions.map((question: mcqQuestion) => {
+				// mix up the options lol
+				const options = [
+					question.option1,
+					question.option2,
+					question.option3,
+					question.answer,
+				].sort(() => Math.random() - 0.5);
+				return {
+					question: question.question,
+					answer: question.answer,
+					options: JSON.stringify(options),
+					gameId: game.id,
+					questionType: "mcq",
+				};
+			});
+
+			await db.question.createMany({
+				data: manyData,
+			});
+		} else if (type === "open_ended") {
+			type openQuestion = {
+				question: string;
+				answer: string;
+			};
+			await db.question.createMany({
+				data: data.questions.map((question: openQuestion) => {
+					return {
+						question: question.question,
+						answer: question.answer,
+						gameId: game.id,
+						questionType: "open_ended",
+					};
+				}),
+			});
+		}
+
+		return NextResponse.json({ gameId: game.id }, { status: 200 });
 	} catch (error) {
-		if (error instanceof ZodError) {
+		if (error instanceof z.ZodError) {
 			return NextResponse.json(
-				{
-					error: error.issues,
-				},
+				{ error: error.issues },
 				{
 					status: 400,
+				}
+			);
+		} else {
+			return NextResponse.json(
+				{ error: "An unexpected error occurred." },
+				{
+					status: 500,
 				}
 			);
 		}
--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -25,12 +25,27 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { BookOpen, CopyCheck } from "lucide-react";
 import { Separator } from "./ui/separator";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface Props {}
 
 type Input = z.infer<typeof quizCreationSchema>;
 
 const QuizCreation = (props: Props) => {
+	const router = useRouter();
+	const { mutate: getQuestions, isPending } = useMutation({
+		mutationFn: async ({ amount, topic, type }: Input) => {
+			const response = await axios.post("/api/game", {
+				amount,
+				topic,
+				type,
+			});
+			return response.data;
+		},
+	});
+
 	const form = useForm<Input>({
 		resolver: zodResolver(quizCreationSchema),
 		defaultValues: {
@@ -41,7 +56,22 @@ const QuizCreation = (props: Props) => {
 	});
 
 	function onSubmit(input: Input) {
-		alert(JSON.stringify(input, null, 2));
+		getQuestions(
+			{
+				amount: input.amount,
+				topic: input.topic,
+				type: input.type,
+			},
+			{
+				onSuccess: ({ gameId }) => {
+					if (form.getValues("type") == "open_ended") {
+						router.push(`/play/open_ended/${gameId}`);
+					} else {
+						router.push(`/play/mcq/${gameId}`);
+					}
+				},
+			}
+		);
 	}
 
 	form.watch(); // rerender when state change
@@ -143,7 +173,9 @@ const QuizCreation = (props: Props) => {
 									Ended
 								</Button>
 							</div>
-							<Button type="submit">Submit</Button>
+							<Button disabled={isPending} type="submit">
+								Submit
+							</Button>
 						</form>
 					</Form>
 				</CardContent>
